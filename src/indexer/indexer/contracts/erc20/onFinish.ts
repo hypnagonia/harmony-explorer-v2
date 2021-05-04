54280feae07832875a001954aaa9910f9f9f2dc2
jenya <hypnagonia@gmail.com>
2021-05-04 21:08:16 +0300

    logs api

--- src/indexer/indexer/contracts/erc20/onFinish.ts
+++ src/indexer/indexer/contracts/erc20/onFinish.ts
@@ -1,35 +1,42 @@
-import {EntityIterator} from 'src/indexer/utils/EntityIterator'
 import {PostgresStorage} from 'src/store/postgres'
 import {ABI} from 'src/indexer/indexer/contracts/erc20/ABI'
 import {logger} from 'src/logger'
+import {Filter} from 'src/types'
 
 const l = logger(module, 'erc20:balance')
 const {call} = ABI
 
+const filter: Filter = {
+  limit: 100,
+  offset: 0,
+  filters: [
+    {
+      property: 'needUpdate',
+      type: 'eq',
+      value: 'true',
+    },
+  ],
+}
 // update balances
 export const onFinish = async (store: PostgresStorage) => {
-  const balancesNeedUpdate = EntityIterator('erc20BalancesNeedUpdate', {
-    index: 0,
-    batchSize: 100,
-    needUpdate: 'true',
-  })
-
   l.info(`Updating balances`)
   let count = 0
-  for await (const b of balancesNeedUpdate) {
-    if (!b.length) {
-      return
+
+  // since we update entries, iterator doesnt work
+  while (true) {
+    const balancesNeedUpdate = await store.erc20.getBalances(filter)
+    if (!balancesNeedUpdate.length) {
+      break
     }
 
-    const promises = b.map(({ownerAddress, tokenAddress}) =>
+    const promises = balancesNeedUpdate.map(({ownerAddress, tokenAddress}) =>
       call('balanceOf', [ownerAddress], tokenAddress).then((balance) =>
         store.erc20.updateBalance(ownerAddress, tokenAddress, balance)
       )
     )
     await Promise.all(promises)
-    count += b.length
+    count += balancesNeedUpdate.length
   }
   l.info(`Updated ${count} balances`)
-
   // todo update holders and total supply
 }
