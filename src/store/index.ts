a210ea32c851490b28b9fc9f8e91c65b237a4213
jenya <hypnagonia>
2021-06-08 18:08:06 +0300

    stores for api

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,17 +3,22 @@ import {config} from 'src/config'
 import {ShardID} from 'src/types/blockchain'
 import {PostgresStorageOptions} from './postgres/types'
 
-const shards: ShardID[] = [0, 1, 2, 3]
+const shards: ShardID[] = [0, 1]
 
-// todo pool for indexer and pool for api
-export const stores = shards.map((shardID) => {
-  const p = config.store.postgres[shardID]
-  return new PostgresStorage({...p, shardID} as PostgresStorageOptions)
-})
+const indexerPools = config.indexer.isEnabled
+  ? shards.map((shardID) => {
+      const p = config.store.postgres[shardID]
+      return new PostgresStorage({...p, shardID} as PostgresStorageOptions)
+    })
+  : null
 
-const stores2 = shards.map((shardID) => {
-  const p = config.store.postgres[shardID]
-  return new PostgresStorage({...p, shardID} as PostgresStorageOptions)
-})
+const APIPools = config.api.isEnabled
+  ? shards.map((shardID) => {
+      const p = config.store.postgres[shardID]
+      return new PostgresStorage({...p, shardID} as PostgresStorageOptions)
+    })
+  : null
 
-export const storesAPI = config.api.isEnabled ? stores2 : stores
+export const stores = indexerPools || APIPools || []
+
+export const storesAPI = APIPools || indexerPools || []
